db793bc147051f7af16dd4b99a46974570ddae54
Mhail027 <mihail.necula@stud.acs.upb.ro>
2024-05-15 09:57:18 +0300

    Add task6.py

---
+++ task6.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import math
+
+print("**** TASK 6 ****")
+
+# Read the data set.
+df = pd.read_csv("../titanic/train.csv")
+
+# Create the list for the column:
+# "Category of age"
+idxs = [] 
+for age in df["Age"]:
+	if np.isnan(age):
+		idxs.append(np.nan)
+		continue
+	if 0 <= age and age < 21:
+		idxs.append(0)
+	if 21 <= age and age < 41:
+		idxs.append(1)
+	if 41 <= age and age < 61:
+		idxs.append(2)
+	if age >= 61:
+		idxs.append(3)
+# Add the column.
+df.insert(12, "Category of age", idxs)
+
+# Find the number of men which survived
+# and in every category of age.
+survs = np.full(4, 0)
+deads = np.full(4, 0)
+men = df.loc[(df["Sex"] == "male")]
+for cage, survived in zip(men["Category of age"], men["Survived"]):
+	if np.isnan(cage) or np.isnan(survived):
+		continue
+	if survived == 1:
+		survs[int(cage)] += 1
+	else:
+		deads[int(cage)] += 1
+
+# Print the number of male survivors from category of age.
+print("Male survivors")
+print(f"[0, 21) years: {survs[0]}")
+print(f"[21, 41) years: {survs[1]}")
+print(f"[41, 61) years: {survs[2]}")
+print(f">= 61 years: {survs[3]}")
+
+# Do a graphic with the procents of amle which survived and died
+# from every category of age
+# Create a graphic with the processed columns.
+fig, (pies) = plt.subplots(2, 2)
+labels = ["D", "S"]
+fig.suptitle("Men")
+
+cnt = [deads[0], survs[0]]
+pies[0, 0].pie(cnt, labels = labels, autopct = '%1.1f%%')
+pies[0, 0].set_title("[0, 21) years")
+
+cnt = [deads[1], survs[1]]
+pies[0, 1].pie(cnt, labels = labels, autopct = '%1.1f%%')
+pies[0, 1].set_title("[21, 41) years")
+
+cnt = [deads[2], survs[2]]
+pies[1, 0].pie(cnt, labels = labels, autopct = '%1.1f%%')
+pies[1, 0].set_title("[41, 61) years")
+
+cnt = [deads[3], survs[3]]
+pies[1, 1].pie(cnt, labels = labels, autopct = '%1.1f%%')
+pies[1, 1].set_title(">= 61 years")
+
+plt.show()
+
+# Other graphic
+
+#labels = ["[0, 21) D", "[0, 21) S", "[21, 41) D", "[21, 41) S", 
+#		  "[41, 61] D", "[41, 61] S", ">= 61 D", ">=61 S"]
+#cnt = [deads[0], survs[0], deads[1], survs[1], deads[2], survs[2],
+#	   deads[3], survs[3]]	   
+#plt.pie(cnt, labels = labels, autopct = '%1.1f%%')
+#plt.show()
