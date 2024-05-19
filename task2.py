4408744d5d7082746e99e08d81332e7ae44373a0
Mhail027 <mihail.necula@stud.acs.upb.ro>
2024-05-19 15:13:40 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -16,8 +16,8 @@ for elem in surv:
 
 # Process the column "Pclass".
 pclass = df["Pclass"]
-pclass_options = set(pclass)
-pclass_counters = np.full(len(pclass_options) + 1, 0)
+pc_options = set(pclass)
+pclass_counters = np.full(len(pc_options) + 1, 0)
 for elem in pclass:
 	pclass_counters[elem] += 1
 
@@ -32,13 +32,12 @@ for elem in sex:
 		sex_counters[1] += 1
 
 # Create a graphic with the processed columns.
-fig, (surv_graphic, pclass_graphic, sex_graphic) = plt.subplots(1, 3)
+fig, (surv_graphic, pc_graphic, sex_graphic) = plt.subplots(1, 3)
 
 surv_graphic.pie(surv_counters, labels = surv_options, autopct = '%1.1f%%')
 surv_graphic.set_title("Survived")
-pclass_graphic.pie(pclass_counters[1:4], labels = pclass_options,
-				   autopct = '%1.1f%%')
-pclass_graphic.set_title("Pclass")
+pc_graphic.pie(pclass_counters[1:4], labels = pc_options, autopct = '%1.1f%%')
+pc_graphic.set_title("Pclass")
 sex_graphic.pie(sex_counters, labels = sex_options, autopct = '%1.1f%%')
 sex_graphic.set_title("Sex")
 
